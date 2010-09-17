b0fc93a66e568cb0d900278d670061e48a1372bb
Sylvain Viollon <thefunny@gmail.com>
2010-09-17 15:45:36 +0200

    Write a clientid that's doesn't rely on Zope 2 things.

--- src/silva/core/cache/clientid.py
+++ src/silva/core/cache/clientid.py
@@ -2,15 +2,35 @@
 # Copyright (c) 2010 Infrae. All rights reserved.
 # See also LICENSE.txt
 
+import random
+import sys
+import hashlib
+import time
+
 from five import grok
 
 from zope.publisher.interfaces.browser import IBrowserRequest
 from zope.session.interfaces import IClientId
+from zope.datetime import rfc1123_date
+
+
+COOKIE_ID = 'silva_session'
 
 
 class ClientId(grok.Adapter):
     grok.context(IBrowserRequest)
     grok.provides(IClientId)
 
+    def create_id(self):
+        return hashlib.sha1(str(random.randrange(sys.maxint))).hexdigest()
+
+    def expire_time(self):
+        return rfc1123_date(time.time() + 12 * 3600)
+
     def __str__(self):
-        return str(self.context.SESSION.id)
+        if COOKIE_ID in self.context.cookies:
+            return self.context.cookies[COOKIE_ID]
+        session_id = self.create_id()
+        self.context.response.setCookie(
+            COOKIE_ID, session_id, path='/', expires=self.expire_time())
+        return session_id

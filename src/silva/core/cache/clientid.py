198cb82ffe0cd9165283bc67f5786ab533fb34c6
Sylvain Viollon <thefunny@gmail.com>
2010-09-17 17:41:01 +0200

    Evil hack.

--- src/silva/core/cache/clientid.py
+++ src/silva/core/cache/clientid.py
@@ -9,7 +9,9 @@ import time
 
 from five import grok
 
+from silva.core.views.interfaces import IVirtualSite
 from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.publisher.browser import TestRequest
 from zope.session.interfaces import IClientId
 from zope.datetime import rfc1123_date
 
@@ -24,13 +26,28 @@ class ClientId(grok.Adapter):
     def create_id(self):
         return hashlib.sha1(str(random.randrange(sys.maxint))).hexdigest()
 
-    def expire_time(self):
+    def expire(self):
         return rfc1123_date(time.time() + 12 * 3600)
+
+    def path(self):
+        return '/'.join(IVirtualSite(self.context).get_root().getPhysicalPath())
 
     def __str__(self):
         if COOKIE_ID in self.context.cookies:
             return self.context.cookies[COOKIE_ID]
         session_id = self.create_id()
         self.context.response.setCookie(
-            COOKIE_ID, session_id, path='/', expires=self.expire_time())
+            COOKIE_ID, session_id, path=self.path(), expires=self.expire())
+        # We set the value in cookies as well in case we are reask to
+        # provide an ID during this same request lifetime.
+        self.context.cookies[COOKIE_ID] = session_id
         return session_id
+
+
+# XXX This should be registered only for testing via a ftesting.zcml
+class TestClientId(grok.Adapter):
+    grok.context(TestRequest)
+    grok.provides(IClientId)
+
+    def __str__(self):
+        return 'client-id'
